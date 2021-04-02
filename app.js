ceb01e09a6e28306e9499ff94ef6b509a48836ab
Sonia Manoubi <manoubisonia@gmail.com>
2021-04-02 17:40:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('./index');
 
-const port = process.env.PORT || 3000,
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, function () {
     console.log("Example app listening at http://%s:%s", "localhost", port)
