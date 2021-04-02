a944775554b4a35cf00b69c6b7450bcc5393d7c5
Sonia Manoubi <manoubisonia@gmail.com>
2021-04-02 16:24:05 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', function (req, res) {
+   res.send('Hello Sonia');
+})
+
+app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
